c1c3f52f105b169ce75c6567be91ea58ebe3ebb2
Dan Wu <danielwu426@gmail.com>
2022-08-17 09:53:13 -0500

    minor styling updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Orca Protocol',
+  title: 'Metropolis',
   //tagline: 'The people first primitive',
   url: 'https://docs.orcaprotocol.org',
   baseUrl: '/',
@@ -17,8 +17,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'itsdanwu.github.io', // Usually your GitHub org/user name.
-  projectName: 'docusaurus-testing', // Usually your repo name.
+  organizationName: 'github.com/orcaprotocol', // Usually your GitHub org/user name.
+  projectName: 'orca-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -67,7 +67,7 @@ const config = {
       },
       navbar: {
         logo: {
-          alt: 'Orca Logo',
+          alt: 'Metropolis Logo',
           src: 'img/logo.svg',
         },
         items: [
